551ed13a9c062d813f1d0469474cf824530479f3
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-03-12 15:51:46 +0800

    Update cms-gw.js

--- functions/cms-gw.js
+++ functions/cms-gw.js
@@ -42,6 +42,7 @@ exports.handler = async (event, context) => {
         	"Access-Control-Allow-Origin": "*",
         	"Access-Control-Allow-Headers": "Content-Type",
         	"Access-Control-Allow-Methods": "GET, POST, OPTION",
+		"Content-Type": "application/json; charset=utf-8"
       	  }
 	};
 };
