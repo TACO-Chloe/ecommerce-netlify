8a671b9dd9aca217d086b4fe1af4c47cff828a62
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-03-12 09:26:55 +0800

    Update cms-gw.js

--- functions/cms-gw.js
+++ functions/cms-gw.js
@@ -5,38 +5,39 @@ const { GraphQLClient, gql } = require('graphql-request');
 //console.log("process.env.GRAPHCMS_ENDPOINT:"+process.env.GRAPHCMS_ENDPOINT);
 
 //console.log("process.env.GRAPHCMS_TOKEN:"+process.env.GRAPHCMS_TOKEN);
-const endpoint = process.env.GRAPHCMS_ENDPOINT;
-const token = process.env.GRAPHCMS_TOKEN;
-
-const graphQLClient = new GraphQLClient(endpoint, {
-headers: {
-  authorization: `Bearer ${token}`,
-},
-});
-
-const query = gql`
-{
-  {
-	  products {
-		name
-		id
-	  }
-  }
-}
-`
 
 
 exports.handler = async (event, context) => {
 
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-	
-  const data = await graphQLClient.request(query);
-  console.log(JSON.stringify(data, undefined, 2));
+	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
 
-  return {
+	const endpoint = process.env.GRAPHCMS_ENDPOINT;
+	const token = process.env.GRAPHCMS_TOKEN;
+
+	const graphQLClient = new GraphQLClient(endpoint, {
+	headers: {
+	  authorization: `Bearer ${token}`,
+	},
+	});
+
+	const query = gql`
+	{
+	  {
+		  products {
+			name
+			id
+		  }
+	  }
+	}
+	`
+	
+	const data = await graphQLClient.request(query);
+	console.log(JSON.stringify(data, undefined, 2));
+
+	return {
 	  statusCode: 200,
 	  //headers,
 	  body: 'test'
 	  //body: JSON.stringify(data, undefined, 2)
-  };
+	};
 };
