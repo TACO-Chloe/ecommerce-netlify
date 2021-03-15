2febf0c313dae6d1c8ce9537e3735620b6a04b08
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-03-15 17:18:27 +0800

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,64 @@
+// exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Hi there Tacos",
+//       event
+//     })
+//   }
+// }
+
+require("dotenv").config();
+
+const { GraphQLClient, gql } = require('graphql-request');
+
+const { Headers } = require('cross-fetch');
+
+global.Headers = global.Headers || Headers;
+
+//console.log("process.env.GRAPHCMS_ENDPOINT:"+process.env.GRAPHCMS_ENDPOINT);
+
+//console.log("process.env.GRAPHCMS_TOKEN:"+process.env.GRAPHCMS_TOKEN);
+
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hi there Tacos",
-      event
-    })
-  }
-}
+
+	// console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+
+	const endpoint = process.env.GRAPHCMS_ENDPOINT;
+	const token = process.env.GRAPHCMS_TOKEN;
+	// console.log("token:"+token);
+	const headers = { authorization: `Bearer ${token}` };
+	//console.log("headers:"+headers);
+
+	const graphQLClient = new GraphQLClient(endpoint, { headers });
+	
+	const {ProductList} = require('./graphql/queries/queries.js');
+	console.log("ProductDetail:"+ ProductDetail);
+	
+	const query = ProductDetail
+
+// 	const query = gql`
+// 	  {
+// 		  products {
+// 			name
+// 			id
+// 		  }
+// 	  }
+// 	`
+	console.log("info:1");
+	const data = await graphQLClient.request(query,id);
+	console.log(JSON.stringify(data, undefined, 2));
+	console.log("info:2");
+
+	return {
+	  statusCode: 200,
+	  body: JSON.stringify(data, undefined, 2),
+	  headers: {
+        	"Access-Control-Allow-Origin": "*",
+        	"Access-Control-Allow-Headers": "Content-Type",
+        	"Access-Control-Allow-Methods": "GET, POST, OPTION",
+		      "Content-Type": "application/json; charset=utf-8"
+      	  }
+	};
+};
