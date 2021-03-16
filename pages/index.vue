be1570dd4b961bdefbcc88140bc4f16a42e0fcb0
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-03-16 16:39:49 +0800

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
       <div class="flex justify-center -mx-4 my-4">
         <router-link
           v-for="product in products"
-          :to="{ name: 'product-slug', params: { id: product.id } }"
+          :to="{ name: 'product-slug', params: { slug: product.id } }"
           :key="product.id"
         >
           <article class="border rounded-md p-6 mx-2">
@@ -43,7 +43,7 @@ import axios from 'axios';
 export default {
   async asyncData() {
     const products = await axios.get("https://admiring-hopper-bcb70e.netlify.app/.netlify/functions/cms-gw", { useCache: true });
-    console.log(JSON.stringify(products));
+	console.log(JSON.stringify(products))
     return products.data;
   },
   components: {
