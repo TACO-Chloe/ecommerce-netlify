18e80961a4ebf124c63e2b519d90407c263a264b
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-03-16 11:37:17 +0800

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -23,39 +23,41 @@ global.Headers = global.Headers || Headers;
 
 exports.handler = async (event, context) => {
 
-	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-	console.log("CONTEXT: \n" + JSON.stringify(context, null, 2))
-	console.log("HTTP-METHOD: \n" + JSON.stringify(event.httpMethod, null, 2))
+	console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+	console.log("CONTEXT: \n" + JSON.stringify(context, null, 2));
+	console.log("HTTP-METHOD: \n" + JSON.stringify(event.httpMethod, null, 2));
 	
+	if (event.httpMethod === "POST") {
 
-	const endpoint = process.env.GRAPHCMS_ENDPOINT;
-	const token = process.env.GRAPHCMS_TOKEN;
-	// console.log("token:"+token);
-	const headers = { authorization: `Bearer ${token}` };
-	//console.log("headers:"+headers);
+		const endpoint = process.env.GRAPHCMS_ENDPOINT;
+		const token = process.env.GRAPHCMS_TOKEN;
+		// console.log("token:"+token);
+		const headers = { authorization: `Bearer ${token}` };
+		//console.log("headers:"+headers);
 
-	const graphQLClient = new GraphQLClient(endpoint, { headers });
-	
-	const {ProductDetail} = require('./graphql/queries/queries.js');
-	console.log("ProductDetail:"+ ProductDetail);
-	
-	const query = ProductDetail
+		const graphQLClient = new GraphQLClient(endpoint, { headers });
 
-	const {id} = {'id':'ckdu44mn40gxh010405uwgbtw'}
-	console.log("info:1");
-	console.log(JSON.stringify({id}, undefined, 2));
-	const data = await graphQLClient.request(query,{id});
-	console.log(JSON.stringify(data, undefined, 2));
-	console.log("info:2");
+		const {ProductDetail} = require('./graphql/queries/queries.js');
+		console.log("ProductDetail:"+ ProductDetail);
 
-	return {
-	  statusCode: 200,
-	  body: JSON.stringify(data, undefined, 2),
-	  headers: {
-        	"Access-Control-Allow-Origin": "*",
-        	"Access-Control-Allow-Headers": "Content-Type",
-        	"Access-Control-Allow-Methods": "GET, POST, OPTION",
-		"Content-Type": "application/json; charset=utf-8"
-      	  }
+		const query = ProductDetail;
+
+		const {id} = event.body;
+		console.log("info:1");
+		console.log(JSON.stringify({id}, undefined, 2));
+		const data = await graphQLClient.request(query,{id});
+		console.log(JSON.stringify(data, undefined, 2));
+		console.log("info:2");
+
+		return {
+		  statusCode: 200,
+		  body: JSON.stringify(data, undefined, 2),
+		  headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Methods": "GET, POST, OPTION",
+			"Content-Type": "application/json; charset=utf-8"
+		  }
+		};
 	};
 };
