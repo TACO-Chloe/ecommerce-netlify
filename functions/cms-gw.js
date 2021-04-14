db89f6b49ab7e0db07869164fbfadb59a7d62fae
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-04-14 15:41:57 +0800

    Update cms-gw.js

--- functions/cms-gw.js
+++ functions/cms-gw.js
@@ -52,7 +52,7 @@ exports.handler = async (event, context) => {
 		console.log("Data:"+postData);
 		gltype = postData.gltype;
 		
-		const GLQuery = require(`./graphql/queriesPost${gltype}`);
+		const GLQuery = require(`./graphql/queriesPost${gltype}.js`);
 		console.log("GLQuery:"+ JSON.stringify(GLQuery));
 		
 		// switch (postData.gltype) {
