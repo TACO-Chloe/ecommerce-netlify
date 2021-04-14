546f3782113a46eec6134094d404c17d30aec205
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-04-14 16:57:39 +0800

    20210414

---
+++ api/graphql/queriesGet.js
@@ -0,0 +1,34 @@
+//import gql from "graphql-tag";
+const gql = require('graphql-tag');
+
+const ProductList = gql`
+	query ProductList {
+		products {
+		    id
+		    name
+		    description{html}
+		    slug
+		    category {
+		      name
+		    }
+		    price
+		    reviews {
+		      name
+		      email
+		      headline
+		      message
+		      rating
+		      product{name}
+		    }
+		    images {
+		      url(transformation: {
+              		image: { resize: { width: 200, height: 200, fit: clip } }
+            		})
+		    }
+		}
+	}
+`;
+
+module.exports = {
+  ProductList
+}
