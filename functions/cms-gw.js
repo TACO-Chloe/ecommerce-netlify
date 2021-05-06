07c776d8f57a8fe710298ac8fe2949d835e14b66
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-05-06 14:49:05 +0800

    Update cms-gw.js

--- functions/cms-gw.js
+++ functions/cms-gw.js
@@ -59,7 +59,8 @@ exports.handler = async (event, context) => {
 	if (event.httpMethod === "POST") {
 		if (event.path === "/.netlify/functions/cms-gw/upload") {
 			const uploadUrl = `${process.env.GRAPHCMS_ENDPOINT}/upload`
-			const buff = Buffer.from(event.body, 'base64');
+			const buff = event.body;
+			//const buff = Buffer.from(event.body, 'base64');
 			//console.log("Buffer:" + buff);
 
 			const contentType = event.queryStringParameters.fileType
