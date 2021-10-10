d1811e3c02251288b1c9f86a6e7af0dd7255c5af
MOMO <46980769+TACO-Chloe@users.noreply.github.com>
2021-10-10 10:36:29 +0800

    Update alipay.js

--- functions/alipay.js
+++ functions/alipay.js
@@ -81,7 +81,7 @@ exports.handler = async (event, context) => {
 		formData.addField('returnUrl', 'https://www.xuexiluxian.cn/success');
 		formData.addField('bizContent', {
 			// outTradeNo: orderId, // 商户订单号,64个字符以内、可包含字母、数字、下划线,且不能重复
-			outTradeNo: new Data().valueOf(),
+			outTradeNo: new Date().valueOf(),
 			// productCode: 'FAST_INSTANT_TRADE_PAY', // 销售产品码，与支付宝签约的产品码名称,仅支持FAST_INSTANT_TRADE_PAY
 			productCode: 'QUICK_WAP_WAY',
 			totalAmount: '0.01', // 订单总金额，单位为元，精确到小数点后两位
